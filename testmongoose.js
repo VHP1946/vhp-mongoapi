ff1242715afd41f5a182ba672a8434200e3c6caf
Ryan Murphy <ryanm@vogelheating.com>
2023-04-25 12:55:05 -0500

    simplified connection method

--- testmongoose.js
+++ testmongoose.js
@@ -4,47 +4,45 @@ const mongoose = require('mongoose'),
 const compschemes = require('./models/CompanySchemes.js');
 
 emps = require('./data/emplog.json');
-//console.log(emps);
 
-mongoose.connect('mongodb+srv://christianv:AMracing5511@cluster0.0awfqdk.mongodb.net/Company?retryWrites=true&w=majority')
-    .then(done => {
-        const Employee = mongoose.model('Employee', compschemes.empSchema);
 
-        /*
-        Employee.deleteMany().then(()=>{
-            Employee.find().then((res)=>{
-                console.log(res);
-            });
-        });
-        */
+connComp = mongoose.createConnection('mongodb+srv://christianv:AMracing5511@cluster0.0awfqdk.mongodb.net/Company');
+const Employee = connComp.model('Employee', compschemes.empSchema);  // One model per connection
 
-        /*
-        Employee.insertMany(emps,{ordered:false})   // ordered:false allows for existing documents without erroring whole function
-            .then((res)=>{
-                console.log(res);
-                Employee.find().then((result)=>{
-                    console.log('Results >', result);
-                });
-            })
-            .catch((err)=>{console.log('error >',err)});
-        */
-        
-        /*
-        Employee.find({empID:null}).then((result)=>{
+
+/*
+Employee.deleteMany().then(()=>{
+    Employee.find().then((res)=>{
+        console.log(res);
+    });
+});
+*/
+
+/*
+Employee.insertMany(emps,{ordered:false})   // ordered:false allows for existing documents without erroring whole function
+    .then((res)=>{
+        console.log(res);
+        Employee.find().then((result)=>{
             console.log('Results >', result);
         });
-        */
-
-        /*
-        Employee.deleteMany({empID:null}).then((result)=>{ 
-            console.log(result);
-        });
-        */
-
-        
-        Employee.find({name:'Ryan Murphy'}).then((result)=>{   //find() always returns an array, findOne() will return single object
-            console.log(result);
-            console.log(result[0].fullName); 
-        });
     })
-    .catch(error => console.log(error));
+    .catch((err)=>{console.log('error >',err)});
+*/
+
+/*
+Employee.find({empID:null}).then((result)=>{
+    console.log('Results >', result);
+});
+*/
+
+/*
+Employee.deleteMany({empID:null}).then((result)=>{ 
+    console.log(result);
+});
+*/
+
+
+Employee.find({name:'Ryan Murphy'}).then((result)=>{   //find() always returns an array, findOne() will return single object
+    console.log(result);
+    console.log(result[0].fullName); 
+});
