d4394056acf59f7656584c5ab96596f6d16b0713
Christian Vogel <christianv@vogelheating.com>
2023-05-05 15:31:42 -0500

    adding console to see result

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ server.on('request',(req,res)=>{
       }
     }
     vmclient.ROUTErequest(vpak.pack).then(result=>{
+      console.log(result);
       res.write(JSON.stringify(result));
       res.end();
     });
