993b41dfca39596b3c5b7efa96f8586141edbaf9
Christian Vogel <christianv@vogelheating.com>
2023-05-05 15:29:23 -0500

    went back to vpak.pack

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ server.on('request',(req,res)=>{
         cip:req.connection.remoteAddress,
       }
     }
-    vmclient.ROUTErequest(vpak.data.pack).then(result=>{
+    vmclient.ROUTErequest(vpak.pack).then(result=>{
       res.write(JSON.stringify(result));
       res.end();
     });
